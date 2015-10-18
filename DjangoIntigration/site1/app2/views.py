c1e3fd231d14d3e6be021315d84b19a52a21e713
mpenumal <mpenumal@asu.edu>
2015-10-17 19:03:32 -0700

    Integrating 2 Views to get hashtag from one and populate the relevant Tweets in JSON format in another view.

--- DjangoIntigration/site1/app2/views.py
+++ DjangoIntigration/site1/app2/views.py
@@ -9,42 +9,31 @@ from .models import Twit
 from models import twitter_parser
 #import pdb; pdb.set_trace()
 from forms import twitterForm
+from forms import indexForm
 from django.core.context_processors import csrf
 
+
 def index(request):
-    twits = Twit.objects.all()
-    t = loader.get_template('app2/index.html')
-    c = Context({'object_list': twits})
-    return HttpResponse(t.render(c))
-	
+	form = indexForm(initial={'jsonout': x})
+	twits = Twit.objects.all()
+	print x
+	args = {}
+	args.update(csrf(request))
+	args['form'] = form
+	return render(request,'app2/index.html', args)
+
+
 def indexSubmit(request):
-	print 'xxxjgj'
 	if request.POST:
-		print 'xxx'
 		form = twitterForm(request.POST)
 		if form.is_valid():
 			string = request.POST['hashtag']
-			print(string)
-			twitter_parser(string)
-			#form.clear()
-			#return HttpResponse('')
+			global x
+			x = twitter_parser(string)
+			return HttpResponseRedirect('app2/index')
 	else:
 		form = twitterForm()
-	
 	args = {}
 	args.update(csrf(request))
 	args['form'] = form
 	return render(request,'app2/indexSubmit.html', args)
-		
-	#args = {}
-	#args['form'] = form
-	#return HttpResponse('hello')
-	#return render_to_response('indexSubmit.html',args)
-
-
-
-# def index(request):
-    # twits = Twit.objects.all()
-    # t = loader.get_template('app2/index.html')
-    # c = Context({'object_list': twits})
-    # return HttpResponse(t.render(c))
