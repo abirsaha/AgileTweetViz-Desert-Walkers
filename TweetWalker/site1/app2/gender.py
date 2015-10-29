3db4923331ce3614d3c727025c38662d5d59e9a9
Jyotinder <jyotinder.pec@gmail.com>
2015-10-29 15:08:16 -0700

    Added last_letter and first_letter in Bayer Classifier

--- TweetWalker/site1/app2/gender.py
+++ TweetWalker/site1/app2/gender.py
@@ -1,21 +1,39 @@
-from nltk.corpus import gutenberg
 import random
-# import FreqDist class
 from nltk import FreqDist
 from nltk.corpus import names
+import nltk.classify.util
 
+def gender_features(word):
+    """
+    Extracting the last_letter and First_letter 
+    of the name
+    """
+    featdict = {}
+    featdict['last_letter'] = word[-1]
+    featdict['first_letter'] = word[0]
+    return featdict
 
-def fun_gender(actor):
+def gender_classifier(actor):
+    """
+        Take name as input using BayerClassifier return the
+        most probable tag(male/female)
+    """
+    
     labeled_names = ([(name, 'male') for name in names.words('male.txt')] +
     [(name, 'female') for name in names.words('female.txt')])
     random.shuffle(labeled_names)
     featuresets = [(gender_features(n), gender) for (n, gender) in labeled_names]
-    train_set, test_set = featuresets[500:], featuresets[:500]
+    train_set = featuresets
+    #print train_set[0]
+    #Training data for the BayerClassifier
     classifier = nltk.NaiveBayesClassifier.train(train_set) 	
-    classifier.classify(gender_features(actor))
+    #print classifier.show_most_informative_features(100)
+    return classifier.classify(gender_features(actor))
   
 def main():
-    fun_gender('Neo')
+    #pass unsupervised data to, find male and female
+    x = gender_classifier('Jyotinder')
+    print x
 
 if __name__ == '__main__':
     main()
