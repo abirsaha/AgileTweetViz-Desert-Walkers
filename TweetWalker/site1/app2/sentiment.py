d9a404360288702006967313a5ed9c3b2bd91c6a
mpenumal <mpenumal@asu.edu>
2015-10-29 17:20:06 -0700

    Sentiment analysis logic for Tweets of any language

---
+++ TweetWalker/site1/app2/sentiment.py
@@ -0,0 +1,28 @@
+"""
+TextBlob is used to process texts.
+It provides a simple API for diving into common natural language processing (NLP)
+    tasks such as part-of-speech tagging, noun phrase extraction, sentiment analysis,
+    classification, translation, and more.
+    https://pypi.python.org/pypi/textblob
+"""
+from textblob import TextBlob
+
+
+def get_sentiment(text):
+    """
+    Gives the polarity of the sentiment associated with the given input text
+    indicating whether it is positive, negative or neutral in '-1 to 1' range
+    :param text: Tweet text
+    :return: Polarity of the sentiment as 0(neutral), 1(positive), or 2(negative)
+    """
+    blob = TextBlob(text)
+    from_lang = blob.detect_language()
+    if from_lang != "en":
+        blob = blob.translate(to="en")
+    sentiment_polarity, sentiment_subjectivity = blob.sentences[0].sentiment
+    if sentiment_polarity > 0.2:
+        return 1
+    elif sentiment_polarity < -0.2:
+        return 2
+    else:
+        return 0
