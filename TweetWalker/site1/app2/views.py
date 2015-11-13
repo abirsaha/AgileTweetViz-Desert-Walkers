72a9c7d5626a69fd454f124e4ad99efb3a010bdf
Jyotinder <jyotinder.pec@gmail.com>
2015-11-13 03:18:18 -0700

    Rabbit que added for geo process in view

--- TweetWalker/site1/app2/views.py
+++ TweetWalker/site1/app2/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.template import Context, loader
+import pika
 # Create your views here.
 
 from .models import Twit
@@ -11,29 +12,25 @@ from models import twitter_parser
 from forms import twitterForm
 from forms import indexForm
 from django.core.context_processors import csrf
-from django.template import RequestContext
 import simplejson as json
 
-
+def create_queue(string):
+    #setup rabbitMQ Connection
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    #set max queue size
+    args = {"x-max-length": 2000}
+    channel.queue_declare(queue='argument_queue', arguments=args)
+    channel.basic_publish(exchange='',
+                            routing_key='argument_queue',
+                            body=string)
 def dashboard(request):
-    print "in dashboard"
-    if request.POST:
-        print "in dashboard post"
-        if 'get_tweets_dashboard' in request.POST:
-            string = request.POST['hashtag_dashboard']
-            y = twitter_parser(string)
-            form = y
-            args = {}
-            args.update(csrf(request))
-            args['form'] = form
-            return render(request,'app2/dashboard.html', args)
-    else:
-        form = x
-        args = {}
-        args.update(csrf(request))
-        args['form'] = form
-        return render(request,'app2/dashboard.html', args)
-
+    print "in index"
+    form = x
+    args = {}
+    args.update(csrf(request))
+    args['form'] = form
+    return render(request,'app2/dashboard.html', args)
 
 def landingpage(request):
     print "in landingpage"
@@ -49,8 +46,9 @@ def landingpage(request):
             form = twitterForm(request.POST)
             if form.is_valid():
                 string = request.POST['hashtagInput']
-                global x
-                x = twitter_parser(string)
+                #global x
+                #x = twitter_parser(string)
+                create_queue(string)
             return HttpResponseRedirect('geo/geomap')
     else:
         form = twitterForm()
